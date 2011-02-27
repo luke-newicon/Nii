39366481ff418230c3c0fae6b8f35f71a54a3270
Steve O'Brien <steve@newicon.net>
2011-02-27 09:07:36 +0000

    updated activation key to be prettier when sent by email

--- protected/modules/user/controllers/RegistrationController.php
+++ protected/modules/user/controllers/RegistrationController.php
@@ -49,8 +49,12 @@ class RegistrationController extends NController
 					$model->contact_id = $contact->id;
 					$model->save();
 					if ($module->sendActivationMail) {
-						$activation_url = $this->createAbsoluteUrl('/user/index/activation',array("activekey" => NData::base64UrlEncode($model->activekey), "e" => NData::base64UrlEncode($model->email)));
-						UserModule::sendMail($model->email,UserModule::t("You registered from {site_name}",array('{site_name}'=>Yii::app()->name)),UserModule::t("Please activate you account go to {activation_url}",array('{activation_url}'=>$activation_url)));
+						$activationUrl = $this->makeActivationLink($model);
+						UserModule::sendMail($model->email,
+							UserModule::t("You registered from {site_name}",
+								array('{site_name}'=>Yii::app()->name)),
+							UserModule::t("Please activate you account go to {activation_url}",
+								array('{activation_url}'=>$activationUrl)));
 					}
 
 					if (($module->loginNotActive ||($module->activeAfterRegister && $module->sendActivationMail==false))
@@ -61,13 +65,18 @@ class RegistrationController extends NController
 						$this->redirect($module->returnUrl);
 					} else {
 						if (!$module->activeAfterRegister && !$module->sendActivationMail) {
-							Yii::app()->user->setFlash('registration',UserModule::t("Thank you for your registration. Contact Admin to activate your account."));
+							Yii::app()->user->setFlash('registration',
+								UserModule::t("Thank you for your registration. Contact Admin to activate your account."));
 						} elseif($module->activeAfterRegister && $module->sendActivationMail==false) {
-							Yii::app()->user->setFlash('registration',UserModule::t("Thank you for your registration. Please {{login}}.",array('{{login}}'=>CHtml::link(UserModule::t('Login'),$module->loginUrl))));
+							Yii::app()->user->setFlash('registration',
+								UserModule::t("Thank you for your registration. Please {{login}}.",
+									array('{{login}}'=>CHtml::link(UserModule::t('Login'),$module->loginUrl))));
 						} elseif($module->loginNotActive) {
-							Yii::app()->user->setFlash('registration',UserModule::t("Thank you for your registration. Please check your email or login."));
+							Yii::app()->user->setFlash('registration',
+								UserModule::t("Thank you for your registration. Please check your email or login."));
 						} else {
-							Yii::app()->user->setFlash('registration',UserModule::t("Thank you for your registration. Please check your email."));
+							Yii::app()->user->setFlash('registration',
+								UserModule::t("Thank you for your registration. Please check your email."));
 						}
 						$this->refresh();
 					}
@@ -76,6 +85,72 @@ class RegistrationController extends NController
 			$this->render('/user/registration',array('model'=>$model,'contact'=>$contact));
 		}
 	}
-
-
+	
+	
+	/**
+	 * Activattion link for user account
+	 */
+	public function actionActivation() {
+		$email = NData::base64UrlDecode($_GET['e']);
+		$activekey = NData::base64UrlDecode($_GET['activekey']);
+		if ($email&&$activekey) {
+			$find = User::model()->notsafe()->findByAttributes(array('email'=>$email));
+			if (isset($find)&&$find->status) {
+			    $this->render('/user/message',array('title'=>UserModule::t("User activation"),'content'=>UserModule::t("You account is active.")));
+			} elseif(isset($find->activekey) && $this->checkActivationKey ($find->activekey, $activekey)) {
+				$find->activekey = crypt(microtime());
+				$find->status = 1;
+				$find->save();
+			    $this->render('/user/message',array('title'=>UserModule::t("User activation"),'content'=>UserModule::t("You account is activated.")));
+			} else {
+			    $this->render('/user/message',array('title'=>UserModule::t("User activation"),'content'=>UserModule::t("Incorrect activation URL.")));
+			}
+		} else {
+			$this->render('/user/message',array('title'=>UserModule::t("User activation"),'content'=>UserModule::t("Incorrect activation URL.")));
+		}
+	}
+	
+	/**
+	 * returns the encoded activation key 
+	 * @param User $u
+	 * @return type 
+	 */
+	public function makeActivationLink(User $u)
+	{
+		// lets take only a few characters 
+		$ak = $this->makeKeyPretty($u->activekey);
+		$key = NData::base64UrlEncode($ak);
+		return $this->createAbsoluteUrl('/user/registration/activation',array(
+			"activekey" =>$key, 
+			"e" => NData::base64UrlEncode($u->email)
+		));
+	}
+	/**
+	 * Check the activation key to ensure the authentisity of this activation
+	 * request
+	 * @param User $u the user to which the key should match
+	 * @param string $urlKey the key recieved by the controller
+	 * @return boolean true the key matches the user and is authorised.
+	 */
+	public function checkActivationKey(User $u, $urlKey)
+	{
+		// need to perform the same processing to the users key as the $urlKey
+		// recieved before it was sent
+		return ($this->makeKeyPretty($u->activekey)==$activekey);
+	}
+	
+	/**
+	 * Dont want the activation key url variable to be a huge link
+	 * So lets only send over a few select characters from the key
+	 * @return type 
+	 */
+	public function makeKeyPretty()
+	{
+		$keyLetrArr = str_split($u->activekey);
+		$compareKeyChars = array(1,5,2,7,3,9,11);
+		$key = '';
+		foreach($compareKeyChars as $index)
+			$key .= $keyLetrArr[$index];
+		return $key;
+	}
 }
