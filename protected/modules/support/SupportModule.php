fa504c8dc4e1d9b1fffbae68aec5b3a39a99b3c8
Steven O'Brien <steve@newicon.net>
2011-03-15 19:17:56 +0000

    added replacement for str_getcsv only available in php >= 5.3

--- protected/modules/support/SupportModule.php
+++ protected/modules/support/SupportModule.php
@@ -132,3 +132,20 @@ class SupportModule extends NWebModule
 	}
 	
 }
+
+
+if (!function_exists('str_getcsv')) {
+
+function str_getcsv($input, $delimiter=',', $enclosure='"', $escape=null, $eol=null) {
+  $temp=fopen("php://memory", "rw");
+  fwrite($temp, $input);
+  fseek($temp, 0);
+  $r = array();
+  while (($data = fgetcsv($temp, 4096, $delimiter, $enclosure)) !== false) {
+    $r[] = $data;
+  }
+  fclose($temp);
+  return $r;
+}
+
+}
