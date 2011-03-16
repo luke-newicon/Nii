81a38549de25fb91456cc82b2071fe66706eee3b
Steven O'Brien <steve@newicon.net>
2011-03-16 18:20:15 +0000

    added hosting module

--- protected/app/config/main.php
+++ protected/app/config/main.php
@@ -38,6 +38,9 @@ return array(
 			'password'=>'bonsan',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+			'generatorPaths'=>array(
+				'ext.gtc',   // Gii Template Collection
+			),
 		),
 		'crm',
 		'kashflow',
