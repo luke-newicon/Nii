47672253d60640d1e0784a6b3379f428a94eaa7e
steven obrien <steve@newicon.net>
2011-04-17 20:40:56 +0100

    active form

--- protected/modules/user/views/admin/create.php
+++ protected/modules/user/views/admin/create.php
@@ -1,4 +1,19 @@
 <?php
+$this->widget('zii.widgets.jui.CJuiTabs', array(
+	'id'=>'userpoptabs',
+	'tabs'=>array(
+		'User'=>$this->renderPartial('_form', array('model'=>$model), true, true),
+		'Permissions'=>'permissions',
+	),
+	// additional javascript options for the tabs plugin
+	'options'=>array(
+		'collapsible'=>false,
+		'create'=>'js:function(){}',
+	),
+	'htmlOptions'=>array('class'=>'solid rounded')
+));
+?>
+<?php
 $this->breadcrumbs=array(
 	UserModule::t('Users')=>array('/user/admin/index'),
 	UserModule::t('Create'),
@@ -6,4 +21,4 @@ $this->breadcrumbs=array(
 ?>
 <h1><?php echo UserModule::t("Add User"); ?></h1>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php //echo $this->renderPartial('_form', array('model'=>$model)); ?>
