713982522ebdcebbc9ea9391262e2f7dc6afea32
Matt Turner <matt.turner@newicon.net>
2011-05-03 16:52:21 +0100

    Padding added to textaremarkdown buttons
    Signed-off-by: Matt Turner <matt.turner@newicon.net>

--- protected/app/views/site/systems.php
+++ protected/app/views/site/systems.php
@@ -6,5 +6,5 @@ $this->breadcrumbs=array(
 
 <h1>Systems</h1>
 <?php $this->widget('modules.nii.widgets.markdown.NMarkdownInput',array('value'=>'tests','name'=>'test')); ?>
-<?php $this->widget('modules.nii.widgets.markdown.NMarkdownInput',array('value'=>'tests','name'=>'test','htmlOptions'=>array('style'=>'width:100%;height:160px;border:1px solid #ccc;margin:0px;border:0px;'))); ?>
-<?php $this->widget('modules.nii.widgets.markdown.NMarkdownInput',array('model'=>$projectTask,'attribute'=>'description','htmlOptions'=>array('style'=>'width:100%;height:160px;border:1px solid #ccc;margin:0px;border:0px;'))); ?>
+<?php $this->widget('modules.nii.widgets.markdown.NMarkdownInput',array('value'=>'tests','name'=>'test')); ?>
+<?php $this->widget('modules.nii.widgets.markdown.NMarkdownInput',array('model'=>$projectTask,'attribute'=>'description')); ?>
