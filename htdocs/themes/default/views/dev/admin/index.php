400d9f29f3cbc7f756a1371ea5730769b4ed5ae3
lukespencer <luke.spencer@newicon.net>
2011-11-09 12:05:05 +0000

    Added jQuery UI development page

--- htdocs/themes/default/views/dev/admin/index.php
+++ htdocs/themes/default/views/dev/admin/index.php
@@ -1,15 +1,16 @@
 <?php $this->widget('nii.widgets.NTabs', 
 	array(
+		'id' => 'DeveloperTabs',
 		'tabs' => array(
 			'Cache'=>array('id'=>'cache', 'content'=>$this->renderPartial('tabs/_cache',null,true)),
 			'Bootstrap'=>array('id'=>'bootstrap', 'content'=>$this->renderPartial('tabs/_bootstrap',null,true)),
 			'Oocss'=>array('id'=>'oocss', 'content'=>$this->renderPartial('tabs/_oocss',null,true)),
+			'jQuery UI'=>array('id'=>'jqueryui', 'content'=>$this->renderPartial('tabs/_jqueryui',null,true)),
 		),
 		'options' => array(
 			'cache' => true,
 		),
 		'htmlOptions' => array(
-			'id' => 'tabs',
 			'class' => 'vertical',
 		),
 //		'title' => 'Developer Tools',
