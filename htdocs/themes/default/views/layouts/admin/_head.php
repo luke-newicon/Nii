b03bdbbe0f8f8767f2e846eb7dca9aa553755b54
Luke Spencer <luke.spencer@newicon.net>
2011-11-09 19:23:58 +0000

    Updates to include CSS for menu bar colours

--- htdocs/themes/default/views/layouts/admin/_head.php
+++ htdocs/themes/default/views/layouts/admin/_head.php
@@ -1,25 +1,61 @@
+<?php $topbarColor = Yii::app()->getModule('admin')->topbarColor ?>
+<?php if ($topbarColor) : ?>
+<?php $topbarColorLighter = NHtml::hexLighter(Yii::app()->getModule('admin')->topbarColor, 8) ?>
+	<style>
+		.topbar-inner, .topbar .fill {
+		  background-color: <?php echo $topbarColor ?>;
+		  background-image: -khtml-gradient(linear, left top, left bottom, from(<?php echo $topbarColorLighter ?>), to(<?php echo $topbarColor ?>));
+		  background-image: -moz-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: -ms-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, <?php echo $topbarColorLighter ?>), color-stop(100%, <?php echo $topbarColor ?>));
+		  background-image: -webkit-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: -o-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='<?php echo $topbarColorLighter ?>', endColorstr='<?php echo $topbarColor ?>', GradientType=0);
+		}
+		.topbar div > ul .menu-dropdown li a:hover,
+		.nav .menu-dropdown li a:hover,
+		.topbar div > ul .dropdown-menu li a:hover,
+		.nav .dropdown-menu li a:hover {
+		  background-color: <?php echo $topbarColor ?>;
+		  background-image: -khtml-gradient(linear, left top, left bottom, from(<?php echo $topbarColorLighter ?>), to(<?php echo $topbarColor ?>));
+		  background-image: -moz-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: -ms-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, <?php echo $topbarColorLighter ?>), color-stop(100%, <?php echo $topbarColor ?>));
+		  background-image: -webkit-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: -o-linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  background-image: linear-gradient(top, <?php echo $topbarColorLighter ?>, <?php echo $topbarColor ?>);
+		  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='<?php echo $topbarColorLighter ?>', endColorstr='<?php echo $topbarColor ?>', GradientType=0);
+		}
+		.topbar div > ul .menu-dropdown,
+		.nav .menu-dropdown,
+		.topbar div > ul .dropdown-menu,
+		.nav .dropdown-menu {
+		  background-color: <?php echo NHtml::hexLighter($topbarColor,10) ?>;
+		}
+		.topbar div > ul .menu-dropdown .divider,
+		.nav .menu-dropdown .divider,
+		.topbar div > ul .dropdown-menu .divider,
+		.nav .dropdown-menu .divider {
+		  background-color: <?php echo $topbarColor ?>;
+		  border-color: <?php echo NHtml::hexLighter($topbarColorLighter,20) ?>;
+		}
+	</style>
+<?php endif; ?>
 <div id="message"></div>
 <div class="head">
-	<?php if(Yii::app()->getModule('admin')->logo) : ?>
+	<?php if (Yii::app()->getModule('admin')->logo) : ?>
 		<div id="sitelogo">
 			<a href="<?php echo Yii::app()->baseUrl; ?>">
 				<img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/<?php echo Yii::app()->getModule('admin')->logo ?>" />
 			</a>
 		</div>
 	<?php endif; ?>
-	<?php if(Yii::app()->getModule('admin')->topbarColor) : ?>
-		<style>
-			.topbar-inner, .topbar .fill{
-				background-color: <?php echo Yii::app()->getModule('admin')->topbarColor ?>;
-				background-image: -moz-linear-gradient(center top , <?php echo Yii::app()->getModule('admin')->topbarColor ?>, <?php echo Yii::app()->getModule('admin')->topbarColor ?>);
-			}
-		</style>
-	<?php endif; ?>
 	<div class="topbar-wrapper">
 		<div class="topbar">
 			<div class="topbar-inner" style="padding-left:20px;padding-right:20px;">
                 <div class="container" style="width:auto">
-					<?php if(Yii::app()->getModule('admin')->menuAppname) : ?>
+					<?php if (Yii::app()->getModule('admin')->menuAppname) : ?>
 						<h3><a href="<?php echo Yii::app()->baseUrl ?>"><?php echo Yii::app()->name ?></a></h3>
 					<?php else : ?>
 						<ul>
@@ -44,8 +80,8 @@
 						</li>
 					</ul>
 					<?php
-					Yii::app()->menus->addDivider('user','User');
-					Yii::app()->menus->addItem('user','Logout',array('/user/account/logout'),'User');
+					Yii::app()->menus->addDivider('user', 'User');
+					Yii::app()->menus->addItem('user', 'Logout', array('/user/account/logout'), 'User');
 					Yii::app()->menus->setUsername(Yii::app()->user->name);
 					$this->widget('nii.widgets.NMenu', array(
 						'items' => Yii::app()->menus->getItems('user'),
