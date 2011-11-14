fa7eac10861974a5c5fd73ea2ab53e4e63dc1efc
Robin Williams <robin.williams@newicon.net>
2011-11-14 12:58:19 +0000

    Updated dashboard view for HfT site. This will need to be branched at some stage, probably...

--- htdocs/themes/default/views/admin/index/dashboard.php
+++ htdocs/themes/default/views/admin/index/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-$this->widget('ext.bootstrap.widgets.menu.BootTabs',array(
+/*$this->widget('ext.bootstrap.widgets.menu.BootTabs',array(
 	'items' => array(
 		array('label'=>'Home', 'url'=>'#', 'active' => true),
 		array('label'=>'Tab 1', 'url'=>'#'),
@@ -7,4 +7,10 @@ $this->widget('ext.bootstrap.widgets.menu.BootTabs',array(
 		array('label'=>'Tab 3', 'url'=>'#'),
 	),
 	'heading' => 'Dashboard',
-));
+));*/ ?>
+<h2>Welcome</h2>
+<p>This is the Hope for Tomorrow development site. As new sections are available, they will appear in the menu above.</p>
+<p>If you spot any bugs or have a feature request, please enter the details below.
+	<br />You can view updates on your requests <a href="https://docs.google.com/a/newicon.net/spreadsheet/ccc?key=0Ape6AGoGde2YdEs2NUlTUzBUZkY4WFVFeE0xZHJlckE" target="_blank">here</a>.
+</p>
+<iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dEs2NUlTUzBUZkY4WFVFeE0xZHJlckE6MQ" width="760" height="719" frameborder="0" marginheight="0" marginwidth="0" style="border-top: 2px solid #dddddd; margin-top: 10px; padding-top: 10px;">Loading...</iframe>
