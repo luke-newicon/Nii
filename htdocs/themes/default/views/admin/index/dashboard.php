535ba20503392a6d4828f2fa2551ac884dfd277f
Robin Williams <robin.williams@newicon.net>
2011-11-28 16:38:13 +0000

    Updated HfT dashboard

--- htdocs/themes/default/views/admin/index/dashboard.php
+++ htdocs/themes/default/views/admin/index/dashboard.php
@@ -9,8 +9,10 @@
 	'heading' => 'Dashboard',
 ));*/ ?>
 <h2>Welcome</h2>
+<div class="alert-message block-message info container pull-left mtm">
 <p>This is the Hope for Tomorrow development site. As new sections are available, they will appear in the menu above.</p>
 <p>If you spot any bugs or have a feature request, please enter the details below.
 	<br />You can view updates on your requests <a href="https://docs.google.com/a/newicon.net/spreadsheet/ccc?key=0Ape6AGoGde2YdEs2NUlTUzBUZkY4WFVFeE0xZHJlckE" target="_blank">here</a>.
 </p>
-<iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dEs2NUlTUzBUZkY4WFVFeE0xZHJlckE6MQ" width="760" height="850" frameborder="0" marginheight="0" marginwidth="0" style="border-top: 2px solid #dddddd; margin-top: 10px; padding-top: 10px;">Loading...</iframe>
+</div>
+<iframe src="https://docs.google.com/spreadsheet/embeddedform?formkey=dEs2NUlTUzBUZkY4WFVFeE0xZHJlckE6MQ" width="760" height="850" frameborder="0" marginheight="0" marginwidth="0" style="margin-top: 10px;">Loading...</iframe>
