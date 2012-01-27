213eeaa3ff9c0f935cb323b9deb53459dba947d4
Dan De Luca <dan.deluca@newicon.net>
2012-01-27 17:58:39 +0000

    renamed column headers on timesheets grid
    Signed-off-by: Dan De Luca <dan.deluca@newicon.net>

--- protected/app/modules/timesheet/views/timesheet/_timesheet.php
+++ protected/app/modules/timesheet/views/timesheet/_timesheet.php
@@ -1,14 +1,14 @@
-<table class="condensed-table bordered-table zebra-striped">
+<table id="timesheet-grid" class="condensed-table bordered-table zebra-striped">
 	<thead>
 		<tr>
 			<th>Task</th>
-			<th>Monday</th>
-			<th>Tuesday</th>
-			<th>Wednesday</th>
-			<th>Thursday</th>
-			<th>Friday</th>
-			<th>Saturday</th>
-			<th>Sunday</th>
+			<th>Mon</th>
+			<th>Tue</th>
+			<th>Wed</th>
+			<th>Thu</th>
+			<th>Fri</th>
+			<th>Sat</th>
+			<th>Sun</th>
 			<th></th>
 		</tr>
 	</thead>
@@ -16,13 +16,13 @@
 		<?php foreach($timesheet->records as $record): ?>
 		<tr>
 			<td>Project</td>
-			<td><input type="text" value="<?php echo $record->time_monday; ?>"/></td>
-			<td><input type="text" value="<?php echo $record->time_tuesday; ?>"/></td>
-			<td><input type="text" value="<?php echo $record->time_wednesday; ?>"/></td>
-			<td><input type="text" value="<?php echo $record->time_thursday; ?>"/></td>
-			<td><input type="text" value="<?php echo $record->time_friday; ?>"/></td>
-			<td><input type="text" value="<?php echo $record->time_saturday; ?>"/></td>
-			<td><input type="text" value="<?php echo $record->time_sunday; ?>"/></td>
+			<td class="hour_units"><input type="text" value="<?php echo $record->time_monday; ?>"/></td>
+			<td class="hour_units"><input type="text" value="<?php echo $record->time_tuesday; ?>"/></td>
+			<td class="hour_units"><input type="text" value="<?php echo $record->time_wednesday; ?>"/></td>
+			<td class="hour_units"><input type="text" value="<?php echo $record->time_thursday; ?>"/></td>
+			<td class="hour_units"><input type="text" value="<?php echo $record->time_friday; ?>"/></td>
+			<td class="hour_units"><input type="text" value="<?php echo $record->time_saturday; ?>"/></td>
+			<td class="hour_units"><input type="text" value="<?php echo $record->time_sunday; ?>"/></td>
 			<td>
 				<a href="<?php echo NHtml::url(array('/timesheet/timesheet/delete','id'=>$record->id))?>" class="icon fam-delete" data-confirm="Are you sure you want to delete this task?"></a>
 			</td>
