82829bb1a7b98fb7dcb83dd1d92940d33020f9be
Dan De Luca <dan.deluca@newicon.net>
2012-01-27 17:58:11 +0000

    Added stylesheet for module
    Signed-off-by: Dan De Luca <dan.deluca@newicon.net>

--- protected/app/modules/timesheet/TimesheetModule.php
+++ protected/app/modules/timesheet/TimesheetModule.php
@@ -5,12 +5,17 @@ class TimesheetModule extends NWebModule {
 	public $name = 'Timesheet';
 	public $description = 'Timesheet goodness';
 	public $version = '0.0.1';
+	public $assetUrl;
 
 	public function init() {
 		Yii::import('timesheet.models.*');
 	}
 	
 	public function setup() {
+		$localPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
+		$this->assetUrl = Yii::app()->assetManager->publish($localPath);
+		Yii::app()->clientScript->registerCssFile($this->assetUrl . '/timesheet.css');
+		
 		Yii::app()->menus->addItem('user', 'Timesheets', array('/timesheet/index/index'),'User');
 	}
 
