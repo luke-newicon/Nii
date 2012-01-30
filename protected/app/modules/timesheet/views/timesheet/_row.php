73fdef8bf9fae00aba8a342c4e7ea32435254378
lukespencer <luke.spencer@newicon.net>
2012-01-30 18:59:44 +0000

    Some updates to the timesheets module
    Signed-off-by: lukespencer <luke.spencer@newicon.net>

--- protected/app/modules/timesheet/views/timesheet/_row.php
+++ protected/app/modules/timesheet/views/timesheet/_row.php
@@ -1,13 +1,14 @@
 <tr>
-	<td>Project</td>
-	<td class="hour_units"><input type="text" value="<?php echo $record->time_monday; ?>"/></td>
-	<td class="hour_units"><input type="text" value="<?php echo $record->time_tuesday; ?>"/></td>
-	<td class="hour_units"><input type="text" value="<?php echo $record->time_wednesday; ?>"/></td>
-	<td class="hour_units"><input type="text" value="<?php echo $record->time_thursday; ?>"/></td>
-	<td class="hour_units"><input type="text" value="<?php echo $record->time_friday; ?>"/></td>
-	<td class="hour_units"><input type="text" value="<?php echo $record->time_saturday; ?>"/></td>
-	<td class="hour_units"><input type="text" value="<?php echo $record->time_sunday; ?>"/></td>
-	<td>
+	<td class="project-column">Project</td>
+	<td class="task-column">Task</td>
+	<td class="hour_units mon-column"><input type="text" value="<?php echo $record->time_monday; ?>" maxlength="2" /></td>
+	<td class="hour_units tue-column"><input type="text" value="<?php echo $record->time_tuesday; ?>" maxlength="2" /></td>
+	<td class="hour_units wed-column"><input type="text" value="<?php echo $record->time_wednesday; ?>" maxlength="2" /></td>
+	<td class="hour_units thu-column"><input type="text" value="<?php echo $record->time_thursday; ?>" maxlength="2" /></td>
+	<td class="hour_units fri-column"><input type="text" value="<?php echo $record->time_friday; ?>" maxlength="2" /></td>
+	<td class="hour_units sat-column"><input type="text" value="<?php echo $record->time_saturday; ?>" maxlength="2" /></td>
+	<td class="hour_units sun-column"><input type="text" value="<?php echo $record->time_sunday; ?>" maxlength="2" /></td>
+	<td class="delete-column">
 		<a href="<?php echo NHtml::url(array('/timesheet/timesheet/delete','id'=>$record->id))?>" class="icon fam-delete" data-confirm="Are you sure you want to delete this task?"></a>
 	</td>
 </tr>
--- protected/app/modules/timesheet/views/timesheet/_timesheet.php
+++ protected/app/modules/timesheet/views/timesheet/_timesheet.php
@@ -1,23 +1,60 @@
 <table id="timesheet-grid" class="condensed-table bordered-table zebra-striped">
 	<thead>
 		<tr>
-			<th>Task Description</th>
-			<th>Mon</th>
-			<th>Tue</th>
-			<th>Wed</th>
-			<th>Thu</th>
-			<th>Fri</th>
-			<th>Sat</th>
-			<th>Sun</th>
-			<th></th>
+			<th class="project-column">Project</th>
+			<th class="task-column">Task</th>
+			<th class="hour_units mon-column">Mon</th>
+			<th class="hour_units tue-column">Tue</th>
+			<th class="hour_units wed-column">Wed</th>
+			<th class="hour_units thu-column">Thu</th>
+			<th class="hour_units fri-column">Fri</th>
+			<th class="hour_units sat-column">Sat</th>
+			<th class="hour_units sun-column">Sun</th>
+			<th class="delete-column"></th>
 		</tr>
 	</thead>
+	<tfoot>
+		<tr id="timesheet-totals">
+			<th colspan="2"></th>
+			<th class="hour_units mon-column">0</th>
+			<th class="hour_units tue-column">0</th>
+			<th class="hour_units wed-column">0</th>
+			<th class="hour_units thu-column">0</th>
+			<th class="hour_units fri-column">0</th>
+			<th class="hour_units sat-column">0</th>
+			<th class="hour_units sun-column">0</th>
+			<th></th>
+		</tr>
+	</tfoot>
 	<tbody>
 		<?php if($timesheet->records): foreach($timesheet->records as $record): ?>
 			<?php $this->renderPartial('_row',array('record' => $record)) ?>
 		<?php endforeach;endif; ?>
 		<tr id="record-add-row">
-			<td colspan="9"><a id="record-add" href="#" class="icon fam-add">Add a record</a></td>
+			<td>
+				<div class="input">
+					<select>
+						<option>Select a Project</option>
+					</select>
+				</div>
+			</td>
+			<td>
+				<div class="input">
+					<select>
+						<option>Select a Task</option>
+					</select>
+				</div>
+			</td>
+			<td class="hour_units mon-column"><input type="text" value="<?php echo $record->time_monday; ?>" maxlength="2" /></td>
+			<td class="hour_units tue-column"><input type="text" value="<?php echo $record->time_tuesday; ?>" maxlength="2" /></td>
+			<td class="hour_units wed-column"><input type="text" value="<?php echo $record->time_wednesday; ?>" maxlength="2" /></td>
+			<td class="hour_units thu-column"><input type="text" value="<?php echo $record->time_thursday; ?>" maxlength="2" /></td>
+			<td class="hour_units fri-column"><input type="text" value="<?php echo $record->time_friday; ?>" maxlength="2" /></td>
+			<td class="hour_units sat-column"><input type="text" value="<?php echo $record->time_saturday; ?>" maxlength="2" /></td>
+			<td class="hour_units sun-column"><input type="text" value="<?php echo $record->time_sunday; ?>" maxlength="2" /></td>
+			<td class="add-column">
+				<a id="record-add" href="#" class="icon fam-add"></a>
+			</td>
 		</tr>
 	</tbody>
 </table>
